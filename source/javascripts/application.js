fc413673753dc2ccfce2bf632f648c5f16c874b7
Will Schenk <wschenk@gmail.com>
2015-06-07 12:59:48 -0400

    New post

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -12,78 +12,104 @@ $( function() {
     $(".comment_universe").slideToggle();
   } );
 
-  // if( $(".sidebar").size() > 0 ) {
-  //   $("[data-offset-top]").attr( "data-offset-top", $(".sidebar").offset().top );
-  // }
-
   $("img[alt=\"left_float\"]").addClass( "left_float" );
   
-  // $.fn.originalTrigger = $.fn.trigger;
-  // $.fn.trigger = function(type, data) {
-  //   console.log(typeof(type)=='string' ? type : type.type, data);
-  //   return $(this).originalTrigger.apply(this,arguments);
-  // }
 
-  $("#search_box").on( 'input', function(data) {
-    var val = $(this).val();
+  $( "#search_box" ).on( 'input', search_typing );
+} );
 
-    if( val == "" ) {
-      $("#search .dropdown").removeClass( "open" );
-    } else {
-      $("#search .dropdown").addClass( "open" );
-      $("#search .dropdown-menu").html("<li>Loading " + val +"</li>")
-      find_article( val ).done( function(results) {
-        var menu = $("#search .dropdown-menu").html("");
-        $.each( results, function( url, info ) {
-          menu.append( "<li><a href=\"" + url + "\">" + info.title + "</a></li>");
-        });
-      });
-    }
-    console.log( "Got " + $(this).val() );
-  })
-});
+var search_typing = function() {
+  var val = $(this).val();
 
-var find_article = function( word ) {
-  console.log( "Started find_article" );
-  var promise = $.Deferred();
-
-  var lookup_word = function( word, promise ) {
-    console.log( "Looking up word:" + word );
-
-    var result = {}
-
-    // var expander = new RegExp( "(.)", "g" );
-    // var matcher = new RegExp( word.replace( expander, "$1.*" ) );
-    var matcher = new RegExp( word );
-
-    $.each( window.articles.index, function( i ) {
-      // console.log( "Looking at " + i );
-      if( matcher.exec( i ) ) {
-        // console.log( "Matched " + i );
-        // console.log( window.articles.index[i] );
-        $.each( window.articles.index[i], function( idx, url ) {
-          // console.log( "Adding " + url );
-          result[url] = window.articles.articles[url];
-        } );
-      }
-    });
-
-    promise.resolve( result );
-
-    console.log( "Lookup over" );
-  }
-
-  if( window.articles === undefined ) {
-    console.log( "Ajax call" );
-    $.get( "/articles.index.json", {cache: true} ).done( function(data) {
-      console.log( "Call done" );
-      window.articles = data;
-      lookup_word( word, promise )
-    } );
+  if( val == "" ) {
+    $("#search .dropdown").removeClass( "open" );
   } else {
-    console.log( "Using existing object" );
-    lookup_word( word, promise );
+    $("#search .dropdown").addClass( "open" );
+    $("#search .dropdown-menu").html("<li class='disabled'><a href='#'>Loading " + val +"</a></li>")
+    find_article( val ).done( function(results) {
+      var menu = $("#search .dropdown-menu").html("");
+      if( $.isEmptyObject( results ) ) {
+        $("#search .dropdown-menu").html("<li class='disabled'><a href='#'>No results for: " + val +"</a></li>")
+      }
+      $.each( results, function( url, info ) {
+        menu.append( "<li><a href=\"" + url + "\">" + info.title + "</a></li>");
+      });
+    });
+  }
+}
+
+var article_index = function() {
+  if( window.article_promise === undefined ) {
+    console.log( "Loading article index" );
+    window.article_promise = $.Deferred();
+
+    $.get( "/articles.index.json" ).done( function(data) {
+      console.log( "Loaded article index" );
+      window.article_promise.resolve( data );
+    } );
   }
 
-  return promise;
+  return window.article_promise;
+}
+
+var find_article = function( search ) {
+  var search_results = $.Deferred();
+
+  article_index().done( function( index ) {
+    // Split the search by widespace
+    var words = search.toLowerCase().replace(/\s+$/, '').split( /\s+/ );
+
+    // Lookup the matches for each word
+    // Note using $.map seems to flatten the result.
+    var full_results = [];
+    $.each( words, function( i, word ) {
+      full_results.push( match_index( word, index ) );
+    } );
+
+    var urls = full_results[0];
+
+    // If there are multiple words, compute the intersection
+    if( full_results.length > 1 ) {
+      var url_counts = {}
+      $.each( full_results, function( i, set ) {
+        $.each( set, function( i, url ) {
+          url_counts[url] = (url_counts[url]||0)+1;
+        } )
+      } );
+
+      urls = [];
+      $.each( url_counts, function( url, count ) {
+        if( count == full_results.length ) {
+          urls.push( url );
+        }
+      } );
+    }
+
+    // Pull in the metadata
+    var results = {};
+    $.each( urls, function( i, url ) {
+      results[url] = index.articles[url];
+    } );
+
+    search_results.resolve( results );
+  });
+
+  return search_results;
+};
+
+var match_index = function( word, index ) {
+  var result = [];
+  var matcher = new RegExp( word );
+
+  $.each( index.index, function( i ) {
+    if( matcher.exec( i ) ) {
+      $.each( index.index[i], function( idx, url ) {
+        if( result.indexOf( url ) < 0 ) {
+          result.push( url );
+        }
+      } );
+    }
+  });
+
+  return result;
 }
