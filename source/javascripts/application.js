029c98b64da44f4b37d4d2da18e54ebb936d5fb3
Will Schenk <wschenk@gmail.com>
2015-06-07 10:09:43 -0400

    Adding search

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -24,4 +24,66 @@ $( function() {
   //   return $(this).originalTrigger.apply(this,arguments);
   // }
 
+  $("#search_box").on( 'input', function(data) {
+    var val = $(this).val();
+
+    if( val == "" ) {
+      $("#search .dropdown").removeClass( "open" );
+    } else {
+      $("#search .dropdown").addClass( "open" );
+      $("#search .dropdown-menu").html("<li>Loading " + val +"</li>")
+      find_article( val ).done( function(results) {
+        var menu = $("#search .dropdown-menu").html("");
+        $.each( results, function( url, info ) {
+          menu.append( "<li><a href=\"" + url + "\">" + info.title + "</a></li>");
+        });
+      });
+    }
+    console.log( "Got " + $(this).val() );
+  })
 });
+
+var find_article = function( word ) {
+  console.log( "Started find_article" );
+  var promise = $.Deferred();
+
+  var lookup_word = function( word, promise ) {
+    console.log( "Looking up word:" + word );
+
+    var result = {}
+
+    // var expander = new RegExp( "(.)", "g" );
+    // var matcher = new RegExp( word.replace( expander, "$1.*" ) );
+    var matcher = new RegExp( word );
+
+    $.each( window.articles.index, function( i ) {
+      // console.log( "Looking at " + i );
+      if( matcher.exec( i ) ) {
+        // console.log( "Matched " + i );
+        // console.log( window.articles.index[i] );
+        $.each( window.articles.index[i], function( idx, url ) {
+          // console.log( "Adding " + url );
+          result[url] = window.articles.articles[url];
+        } );
+      }
+    });
+
+    promise.resolve( result );
+
+    console.log( "Lookup over" );
+  }
+
+  if( window.articles === undefined ) {
+    console.log( "Ajax call" );
+    $.get( "/articles.index.json", {cache: true} ).done( function(data) {
+      console.log( "Call done" );
+      window.articles = data;
+      lookup_word( word, promise )
+    } );
+  } else {
+    console.log( "Using existing object" );
+    lookup_word( word, promise );
+  }
+
+  return promise;
+}
