7f601de92875b0f5415dc7509a500fc6841a2b28
Will Schenk <wschenk@gmail.com>
2015-07-24 22:07:57 -0400

    Automated commit at 2015-07-25 02:07:57 UTC by middleman-deploy 1.0.0

---
+++ javascripts/admin/components/dashboard-b27a29eb.js
@@ -0,0 +1,1 @@
+(function(){this.Dashboard=React.createClass({render:function(){return React.createElement("div",{className:"dashboard"},React.createElement("div",{className:"row"},React.createElement(DashboardDraftList,null),React.createElement(DashboardPublishedList,null)))}})}).call(this);
