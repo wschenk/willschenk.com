ae8f7387510c0247af55ebe30b0fc3549130f345
Will Schenk <wschenk@gmail.com>
2020-06-03 09:05:48 -0400

    tezlab draft

--- content/articles/2020/tezlab_api/api.js
+++ content/articles/2020/tezlab_api/api.js
@@ -1,93 +1,63 @@
 const COOKIE_AUTH_TOKEN = 'tezlab_token';
 
-function loadProfile( success, error ) {
-    const profile = cacheGet( 'profile' );
-    if( profile ) {
-        success( profile );
+function cacheGetFromEndpoint( key, endpoint, success, error ) {
+    let data = JSON.parse( localStorage.getItem( key ) );
+    if( data ) {
+        success( data );
         return;
     }
-    
-    if( cacheGet( COOKIE_AUTH_TOKEN ) ) {
-        fetchGet( '/v1/profile' )
-            .then( res => res.json() )
-            .then( json => {
-                if( json.success == false ) {
-                    error( json );
-                } else {
-                    cacheSet( 'profile', json );
-                    success( json );
-                }})
-            .catch( err => console.log( err ) );
-    } else {
-        error( {errors: 'Please log in'} );
+
+    const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
     }
+    
+    const authToken = JSON.parse( localStorage.getItem( COOKIE_AUTH_TOKEN ) );
+    if (authToken) {
+        headers['Authorization'] = `Token token=${authToken}`;
+    }
+    
+    const fetchInit = {
+        method: 'GET',
+        headers,
+        credentials: 'include'
+    }
+    fetch(endpoint, fetchInit)
+        .then( res => res.json())
+        .then( json => {
+            if( json.success == false ) {
+                error( json );
+            } else {
+                localStorage.setItem( key, JSON.stringify( json ) );
+                success( json );
+            }
+        })
+        .catch( err => {
+            console.log( err );
+            error( err );
+        })
+}
+
+function loadProfile( success, error ) {
+    cacheGetFromEndpoint( 'profile', '/v1/profile', success, error );
 }
 
 function login( email, password, success, error ) {
-    fetchPost(`/v1/token`, JSON.stringify({ email, password }))
-        .then(res => res.json())
-        .then(json => {
-            if( json.success == false ) {
-                error( json )
-            } else {
-                cacheSet( COOKIE_AUTH_TOKEN, json.user.user_token );
-                success( json );
-            }})
-        .catch(err => console.log( err ) );
-}
-
-function fetchPost( endpoint, body ) {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const fetchInit = {
         method: 'POST',
         headers,
-        body,
+        body: JSON.stringify( {email, password} )
     };
-    return fetch(endpoint, fetchInit);
-};
-      
-function fetchGet(endpoint) {
-    const headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-    }
-    const authToken = cacheGet( COOKIE_AUTH_TOKEN );
-    if (authToken) {
-        headers['Authorization'] = `Token token=${authToken}`;
-    }
-    const fetchInit = {
-        method: 'GET',
-        headers,
-        credentials: 'include',
-    }
-    return fetch(endpoint, fetchInit);
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return undefined;
-}
-
-function cacheGet( key ) {
-    const value = localStorage.getItem( key );
-    if( value ) {
-        return JSON.parse( value );
-    }
-    return undefined;
-}
-
-function cacheSet( key, value ) {
-    localStorage.setItem( key, JSON.stringify( value ) );
+    fetch(`/v1/token`, fetchInit )
+        .then(res => res.json())
+        .then(json => {
+            if( json.success == false ) {
+                error( json )
+            } else {
+                localStorage.setItem( COOKIE_AUTH_TOKEN, JSON.stringify( json.user.user_token ) );
+                success( json );
+            }})
+        .catch(err => console.log( err ) );
 }
