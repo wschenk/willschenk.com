e3cbee937324c55be065a4e01a2e0ad538b5b7ca
Will Schenk <wschenk@gmail.com>
2020-07-30 06:40:32 -0400

    SSB drafts and more work on the signalling server

--- content/articles/2020/coding_with_ssb/pull_test.js
+++ content/articles/2020/coding_with_ssb/pull_test.js
@@ -1,5 +1,6 @@
 const pull = require('pull-stream')
 
+/*
 const range = ( source, closure ) => {
     let done = false;
     while( !done ) {
@@ -12,5 +13,64 @@ const range = ( source, closure ) => {
         })
     }
 }
+*/
+function range(source) {
+    return new Promise( (resolve, reject) => {
+        let values = []
+        pull( source,
+              pull.drain( (e) => values.push(e), (err) => {
+                  if( err ) {
+                      reject(err);
+                  } else {
+                      resolve(values);
+                  }
+              }))
+    })
+}
 
-range( pull.values( [1,2,3,4] ), (e) => console.log( e ) )
+(async () => {
+    values = await range( pull.values([1,2,3,4]) )
+    console.log( values );
+})()
+
+const ssb = require('ssb-client')
+
+async function to_a( source ) {
+    return new Promise( (resolve, reject) => {
+        let values = [];
+        pull( source,
+              pull.drain( (v) => {
+                  values.push(v)
+              },
+                          (err) => {
+                              console.log( "Done" );
+                              if( err ) {
+                                  reject(err);
+                              } else {
+                                  resolve(values);
+                              }
+                          } )
+            )
+    })
+}
+
+ssb( async (err, sbot) =>  {
+    if( err ) {
+        console.log( err );
+        process.exit(1);
+    }
+
+    const identity = sbot.id;
+
+    contacts = sbot.links( {
+        source: identity,
+        rel: 'contact',
+        values: true
+    })
+
+    console.log( "Looping over contacts" )
+    v = await to_a( contacts )
+    console.log( `Found ${v.length} entries` );
+
+    process.exit(0);
+} )
