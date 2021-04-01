21e26c61d0705cb1b8c6beeb89762974d8439f87
Will Schenk <wschenk@gmail.co>
2021-04-01 17:12:23 -0400

    Drafts

---
+++ content/articles/2021/exploring_a_git_repository/entry_point.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+REPO_WORK_DIR=/repository
+WORK_DIR=/output
+
+if [ -d ${REPO_WORK_DIR} ]; then
+   echo Using repo in ${REPO_WORK_DIR}
+else
+    if [ -z "$REPO" ]; then
+        echo Please set the REPO env variable or mount ${REPO_WORK_DIR}
+        exit 1
+    fi
+
+    git clone $REPO ${REPO_WORK_DIR}
+fi
+
+if [ ! -d ${WORK_DIR} ]; then
+    echo Creating ${WORK_DIR}
+    mkdir -p ${WORK_DIR}
+fi
+
+
+# Create a log of commits
+(cd ${REPO_WORK_DIR};git log --reverse --pretty='format:%aI|%ae|%an|%D') | sort > ${WORK_DIR}/commits.log
+
+# Create a list of authors
+
+(cd ${REPO_WORK_DIR};git log --pretty=format:"%ae:%an") | sort -u > ${WORK_DIR}/author.log
+
+
+cd ${WORK_DIR}
+
+for i in /app/*rb; do
+    ruby $i
+done
---
+++ content/articles/2021/exploring_a_git_repository/monthly_commits.rb
@@ -0,0 +1,42 @@
+require 'csv'
+require 'json'
+require 'date'
+
+count = 0
+last_month = nil
+people = {}
+
+CSV.open( ARGV[1] || "monthly_commits.csv", "w" ) do |csv|
+  csv << ['date', 'commits', 'authors', 'authors_json' ]
+
+  File.readlines( ARGV[0] || "commits.log" ).each do |line|
+    date, email, name = line.split( "|" )
+    month = date.gsub( /-\d\dT.*/, "" ).chomp
+
+    if month != last_month && last_month
+      csv << [last_month, count, people.length, people.to_json]
+
+      count = 0
+      people = {}
+
+      lc = 0
+
+      # Fill in the missing months with zeros
+      next_month = (Date.parse( "#{last_month}-01" ) >> 1).strftime( "%Y-%m" )
+      while next_month != month
+        csv << [next_month, 0, 0, {}.to_json]
+        next_month = (Date.parse( "#{next_month}-01" ) >> 1).strftime( "%Y-%m" )
+        lc += 1
+        continue if lc > 24 # after 2 years this is probably broken
+      end
+    end
+
+    people[name] ||= 0
+    people[name] += 1
+
+    count += 1
+    last_month = month
+  end
+
+  csv << [last_month, count, people.length, people.to_json]
+end
