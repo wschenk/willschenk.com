af9320babf05e1b1860d50155d1e5fe2e3d8b62d
Will Schenk <wschenk@gmail.com>
2024-03-26 09:49:57 -0400

    Updated instructions

--- content/labnotes/2024/deploying_puppeteer_on_fly.io/screenshot.js
+++ content/labnotes/2024/deploying_puppeteer_on_fly.io/screenshot.js
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer';
 (async () => {
     const browser = await puppeteer.launch({
         headless: true,
+        // executablePath: "/usr/bin/google-chrome",
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
     
