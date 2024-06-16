4d30be5a08075cf9732057dbe87c6cb3881614f6
Will Schenk <wschenk@gmail.com>
2024-06-16 13:54:11 -0400

    Added compression and qr code

--- content/howto/2024/recreating_notepadtab/main.js
+++ content/howto/2024/recreating_notepadtab/main.js
@@ -4,6 +4,27 @@ import './main.css';
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace/dist/components/copy-button/copy-button.js';
 import '@shoelace-style/shoelace/dist/components/textarea/textarea.js';
+import '@shoelace-style/shoelace/dist/components/qr-code/qr-code.js';
+import pako from 'pako';
+
+function deflateToBase64( inputData ) {
+    const compressed = pako.deflate( inputData );
+    const base64 = window.btoa( String.fromCharCode.apply(null, compressed ));
+
+    return base64;
+}
+
+function inflateFromBase64( base64 ) {
+    const reverseBase64 = atob(base64);
+
+    const reverseBase64Array = new Uint8Array(reverseBase64.split("").map(function(c) {
+        return c.charCodeAt(0); }));
+
+    const inflatedRaw = pako.inflate( reverseBase64Array );
+    const decompressed = String.fromCharCode.apply( null, inflatedRaw );
+
+    return decompressed;
+}
 
 window.addEventListener("load", (event) => {
     const box = document.getElementById( "note-area" )
@@ -13,14 +34,18 @@ window.addEventListener("load", (event) => {
     const hash = window.location.hash
     if( hash != '' ) {
         // Restore the value
-        const smaller = window.atob( hash.substring( 1 ) )
+        const smaller = inflateFromBase64( hash.substring( 1 ) )
         box.value = smaller;
     }
 
+    qrcode.value = window.location.href
+    qrcode.size = 256;
+    
     box.addEventListener( "sl-input", (event) => {
         // Update the url on change
-        const hash = window.btoa( box.value )
+        const hash = deflateToBase64( box.value )
         window.location.hash = '#' + hash
         copy.value = window.location
+        qrcode.value = window.location.href
     });
 });
