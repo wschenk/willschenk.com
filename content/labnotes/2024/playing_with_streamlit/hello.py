b67ea1145ab6b4338996d46c3817088acc6c02cb
Will Schenk <wschenk@gmail.com>
2024-08-21 13:55:14 -0400

    Streamlit

---
+++ content/labnotes/2024/playing_with_streamlit/hello.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
